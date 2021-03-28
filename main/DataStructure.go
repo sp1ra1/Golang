158539a98312c32a76d4cf1fa4f917230814c7dc
jingtzhang <jingtzhang@lm-shc-16503067.corp.ebay.com>
2021-03-28 17:12:01 +0800

    function closure requires review, go to methods and interface learning

--- main/DataStructure.go
+++ main/DataStructure.go
@@ -20,16 +20,16 @@ func structInit() {
 	p1 := Pair{1, 2}
 	p2 := Pair{row: 1, col: 2}
 	p3 := Pair{}
-	p4 := &Pair{1,2}
-	fmt.Println(p1, p2, p3, p4)
+	p4 := &Pair{1, 2}
+	fmt.Println(p1, p2, p3, *p4)
 }
 
-func arrays()  {
+func arrays() {
 	var a [2]int
 	a[0] = 0
 	a[1] = 1
 	fmt.Println(a)
-	primes := [6]int{1,2,3,5,7,11}
+	primes := [6]int{1, 2, 3, 5, 7, 11}
 	fmt.Println(primes)
 }
 
@@ -57,11 +57,11 @@ func sliceVerusArray() {
 	// This is an array
 	var array = [3]int{1, 2, 3}
 	// This is a slice (describe a section of underlying data)
-	var slice = []int{4,5}
-	s := []struct{
+	var slice = []int{4, 5}
+	s := []struct {
 		row int
 		col int
-	} {
+	}{
 		{1, 2},
 		{3, 4},
 	}
@@ -70,18 +70,19 @@ func sliceVerusArray() {
 	fmt.Println(s)
 }
 
+// todo
 func sliceLengthAndCap() {
 	s := []int{1, 2, 3, 4, 5}
-	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s);
+	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s)
 
 	s = s[:0]
-	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s);
+	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s)
 
 	s = s[:4]
-	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s);
+	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s)
 
 	s = s[2:]
-	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s);
+	fmt.Printf("len = %d, cap = %d, %v\n", len(s), cap(s), s)
 
 	var a []int
 	fmt.Println(a, len(a), cap(a))
@@ -90,9 +91,72 @@ func sliceLengthAndCap() {
 	}
 }
 
+// make(type, len, cap)
 func makeSlice() {
 	a := make([]int, 5)
 	fmt.Println(a)
+
+	b := make([]bool, 5, 10)
+	fmt.Println(len(b), cap(b))
+}
+
+// append(s []T, ...T) []T
+func appendSlice() {
+	var s []string
+	s = append(s, "gg")
+	s = append(s, "my", "friend")
+	fmt.Println(s)
+}
+
+func rangeStatement() {
+	a := []int{1, 2, 3, 4, 5, 6}
+	for i, v := range a {
+		fmt.Println(i, v)
+	}
+
+	for _, v := range a {
+		fmt.Println(v)
+	}
+}
+
+func mapDemo() {
+	var a map[string]bool
+	a = make(map[string]bool)
+	a["gg"] = true
+	fmt.Println(a["gg"])
+
+	var m = map[string]float32{
+		"gg":     3.0,
+		"friend": 4.0,
+	}
+	fmt.Println(m)
+}
+
+func mapOperation() {
+	m := make(map[string]int)
+	m["answer"] = 42
+	delete(m, "answer")
+	// if the key is in the map, return the corresponding value and true, else return zero value of the type and false
+	v, ok := m["answer"]
+	fmt.Println(v, ok)
+}
+
+func funcParameter(x, y float64) float64 {
+	return x + y
+}
+
+func wrapper(f func(float64, float64) float64) float64 {
+	return f(3.0, 4.0)
+}
+
+// function closure
+// each closure is bound to its own "sum" variable
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
 
 func main() {
@@ -112,4 +176,20 @@ func main() {
 	sliceVerusArray()
 	sliceLengthAndCap()
 	makeSlice()
+	appendSlice()
+
+	rangeStatement()
+
+	mapDemo()
+	mapOperation()
+
+	fmt.Println(wrapper(funcParameter))
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
